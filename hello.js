8446e35b8e111ef16505a2d4a2e9b479af553c23
zhangchunlai <1810468029@qq.com>
2016-05-11 12:42:02 +0800

    hello

---
+++ hello.js
@@ -0,0 +1,41 @@
+/ alert("Hi");
++
++
++// dfdfdf
++
++// dfdfd
++
++// dfdfd
++
++
++
++
++// var 性别 = '女';
++// alert(性别);
++
++var age = 90;
++// alert(20 == '20');
++
++// {
++// alert(age + 3);
++// alert("13" + "广告");
++// }
++
++if (age > 40) {
++ if (age >= 90) { alert("耄耋之年"); } else {
++ alert("四十不惑");
++ }
++} else {
++ alert("人生的路还很长");
++}
++
++for (var i = 0; i < 100; i++) {
++ console.log(i)
++ // 在控制台输出信息
++}
++
++function sayHello(name) {
++ console.log(name + ", 你好");
++}
++
++sayHello("杨老师");
