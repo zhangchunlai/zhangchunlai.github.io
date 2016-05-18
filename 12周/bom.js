ba6c356867949ac738e49395b6a66b68be51e79f
zhangchunlai <1810468029@qq.com>
2016-05-18 11:46:21 +0800

    作业

---
+++ 12周/bom.js
@@ -0,0 +1,12 @@
+// 对象的声明及使用
+
+var person = {
+    name: 'mr.right',
+    gender: 'male',
+    age: 20,
+    sing: function() {
+        console.log('正在上跑步')；
+    }
+}
+console.log(person.name);
+person.running();
